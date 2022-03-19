e1ddb06edaf4930650d3bbc64428629c16cf80fd
Jasonnavarro86 <jasonnavarro86@yahoo.com>
2022-03-19 05:20:11 -0400

    Testing take 2

--- my-coffee-shop/pages/_app.js
+++ my-coffee-shop/pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer>
+      2022 @Jason Navarro 
+      </footer>
+    </div>
+  )
 }
 
 export default MyApp
